be152e989b050352f1d7c02b36765ee5f860b6bf
stoycho <stoycho@cmail.ch>
2020-06-09 14:25:03 +0300

    Remove getId() function

--- projects/TodoApp/src/app/todo-serves.service.ts
+++ projects/TodoApp/src/app/todo-serves.service.ts
@@ -18,7 +18,7 @@ export class TodoServesService {
 
   getTodosObservable(): Observable<TodoObject[]> {
     return this.http.get<TodoObject[]>(this.todosUrl).pipe(
-      tap( x => console.dir(x))
+      catchError( err => this.handleHttpError(err))
     );
   }
 
@@ -58,23 +58,5 @@ export class TodoServesService {
     return this.todos.findIndex((a: TodoObject) => {
       return (a.id == todoId);
     })
-  }
-
-  private getId(): number {
-    let id: number = 1;
-    this.todos.sort((a: TodoObject, b: TodoObject) => {
-      if (a.id < b.id) {
-        return -1;
-      } else if (a.id === b.id) {
-        return 0;
-      } else {
-        return 1;
-      }
-    }).forEach(element => {
-      if (element.id === id) {
-        id = id + 1;
-      }
-    });
-    return id;
   }
 }
